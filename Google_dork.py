4b9fbe58b1fb0fcfc11841810e13f9b6dce6eb9c
Laurent <Laurent-Andrieu@users.noreply.github.com>
2020-08-24 18:42:55 +0200

    Calcul des requêtes necessaires + ajout book type

--- Google_dork.py
+++ Google_dork.py
@@ -3,10 +3,11 @@ import os
 import urllib.request
 import requests
 
+
 # Directories
 USER = os.getlogin()
 PATH = f'C:/Users/{USER}/Downloads/'
-DEST_PATH = f'C:/Users/{USER}/PycharmProjects/'
+DEST_PATH = f'C:/Users/{USER}/PycharmProjects/Google_dork/'
 # API
 ENGINE_ID = ''
 API_KEY = ''
@@ -16,7 +17,7 @@ class Listfiles:
     """
 
     La classe 'Listfiles' permets de vérifier et les extensions de fichiers présents
-    et de converver leur noms dans un objet.
+    et de conserver leurs noms dans un objet.
     """
 
     def __init__(self, path: str, ext: [str, list]):
@@ -55,7 +56,7 @@ class SearchApi:
     Cette class permet d'utiliser l'API 'Programmable Search Engine' de Google.
 
     La méthode 'search' renvoie un dictionnaire des résultats trouvés sous la forme d'un titre: URL.
-    La fonction '__add__' permet de conserver et mettre à jour les nouveau fichiers trouvés par la recherche.
+    La fonction '__add__' permet de conserver et mettre à jour les nouveaux fichiers trouvés par la recherche.
     """
 
     def __init__(self, engine_id: str, api_key: str):
@@ -67,7 +68,6 @@ class SearchApi:
         self.engine_id = engine_id
         self.api_key = api_key
         self.subject = None
-        self.start = 0
         self.query = None
         self.URL = f''
         self.ext = None
@@ -76,9 +76,9 @@ class SearchApi:
     def __add__(self, title: str, link: str):
         """
 
-        :param title: titre contenu de la recherche
-        :param link: URL du contenu de la recherche
-        :return: void
+        :param title: Titre contenu de la recherche.
+        :param link: URL du contenu de la recherche.
+        :return: Void.
         """
         self.results[title] = link
         if not os.stat('result.json').st_size == 0:
@@ -90,36 +90,57 @@ class SearchApi:
                 json.dump(self.results, docs)
                 docs.close()
 
-    def search(self, topic: str, filetype: [str, list], quantity: int):
+    def search(self, topic: str, filetype: [str, list], quantity: int, country: str, is_book=True):
         """
 
-        :param topic: mot clé de la recherche
-        :param filetype: extension du/des fichier(s) souhaité(s)
-        :param quantity: nombre de résultats à rechercher
-        :return: dictionnaire des résultats, titre: URL
+        :param topic: Mot clé de la recherche.
+        :param filetype: Extension du/des fichier(s) souhaité(s).
+        :param quantity: Nombre de résultats à rechercher.
+        :param country: Zone géographique de la recherche.
+        :param is_book: Recherche de livres uniquements.
+        :return: Dictionnaire des résultats, titre: URL.
         """
-
-        if isinstance(filetype, list):
-            filetype = '|'.join(i for i in filetype)
-            print(filetype)
-        self.query = f'filetype:{filetype} {topic}'
-        self.URL = f"https://www.googleapis.com/customsearch/v1?key={self.api_key}&cx={self.engine_id}&q={self.query}&start={self.start}&num={quantity}"
-
         if not all(isinstance(s, str) for s in filetype):
             raise Exception(f'Extension type Error: {self.ext} should be a str list')
 
-        # GET REQUEST API
-        data = requests.get(self.URL).json()
-        items = data.get('items')
-        try:
-            for i, content in enumerate(items, start=self.start):
-                title = content.get("title")
-                snippet = content.get("snippet")
-                link = content.get("link")
-                self.__add__(title, link)
-            return self.results
-        except TypeError:
-            raise Exception(data['error']['message'])
+        # Extensions multiples
+        if isinstance(filetype, list):
+            filetype = '|'.join(i for i in filetype)
+            print(filetype)
+
+        # Précision de la recherche
+        if not is_book:
+            self.query = f'filetype:{filetype}%20{topic}'
+        else:
+            self.query = f'book:{topic} filetype: {filetype}'
+
+        # Compteur de requêtes
+        c = int((quantity + 9) / 10)
+        for i in range(c):
+            # Déterminer le start
+            start = 1 + ((c - 1) * 10)
+            # Déterminer le num
+            num = int(((quantity / 10) % 1) * 10)
+            if num == 0:
+                num += 10
+            self.URL = f"https://www.googleapis.com/customsearch/v1?key={self.api_key}" \
+                       f"&cx={self.engine_id}&q={self.query}&start={start}&num={num}&cr=country{country}"
+
+            # GET REQUEST API
+            data = requests.get(self.URL).json()
+            items = data.get('items')
+            try:
+                if items:
+                    for item, content in enumerate(items):
+                        title = content.get("title")
+                        snippet = content.get("snippet")
+                        link = content.get("link")
+                        self.__add__(title, link)
+                    return self.results
+                else:
+                    raise Exception(data['error']['message'])
+            except TypeError:
+                raise Exception(data['error']['message'])
 
 
 def download(href: [str, list]):
@@ -160,9 +181,9 @@ def download(href: [str, list]):
 
 def main():
     web = SearchApi(ENGINE_ID, API_KEY)
-    files = web.search('python', ['pdf', 'txt'], 12)
+    files = web.search('python', 'pdf', 2, 'FR')
     href = [files[c] for i, c in enumerate(files)]
-    download(href)
+    # download(href)
 
 
 if __name__ == '__main__':
